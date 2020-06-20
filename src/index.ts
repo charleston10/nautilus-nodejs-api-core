aed6b9795886ecf95a78df12e9df8573777f3494
charleton10 <charleston.anjos@gmail.com>
2020-06-20 11:00:19 -0300

    Create index for import

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import UseCase from './base/UseCase';
+import BaseRepository from "./base/BaseRepository";
+
+import { container, application, database, logger } from "./Container";
+
+export {
+    container,
+    application,
+    database,
+    logger,
+    BaseRepository,
+    UseCase,
+}
